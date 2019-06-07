e968867962f76ac2d99e48b1ad57f9650f2614f9
Roza Jiang <junzhujiang@gmail.com>
2019-06-08 09:48:01 +1000

    Added basic interaction methods on LoginPage

--- client/src/components/LoginPage/LoginPage.ui.tsx
+++ client/src/components/LoginPage/LoginPage.ui.tsx
@@ -2,7 +2,38 @@ import * as React from 'react'
 import './styles.css'
 import { Container, Grid, TextField, Typography, Button } from '@material-ui/core'
 
-export class LoginPage extends React.Component<any, any> {
+interface LoginPageState {
+    email: string,
+    password: string,
+}
+
+export class LoginPage extends React.Component<any, LoginPageState> {
+
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleOnEmailChange(event: React.SyntheticEvent){
+        let currentTarget = event.currentTarget as HTMLInputElement
+        this.setState({
+            email: currentTarget.value
+        })
+    }
+
+    handleOnPasswordChange(event: React.SyntheticEvent){
+        let currentTarget = event.currentTarget as HTMLInputElement
+        this.setState({
+            password: currentTarget.value
+        })
+    }
+
+    handleOnSignin(event: React.SyntheticEvent){
+        console.log(this.state)
+    }
 
     public render() {
         return (
@@ -14,28 +45,31 @@ export class LoginPage extends React.Component<any, any> {
                     <Grid container spacing={1}>
                         <Grid item xs={12}>
                             <TextField
-                                id="outlined-name"
+                                id="outlined-email"
                                 label="Email"
                                 margin="normal"
                                 variant="outlined"
+                                onChange={(e) => this.handleOnEmailChange(e)}
                             />
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
-                                id="outlined-name"
+                                id="outlined-password"
                                 label="Password"
                                 margin="normal"
                                 variant="outlined"
                                 type="password"
+                                onChange={(e) => this.handleOnPasswordChange(e)}
                             />
                         </Grid>
                         <Grid item xs={12}>
                             <Button
-                            className={'sign-in-button'}
-                            style={{margin: '15px'}}
-                            variant="contained"
-                            color="primary"
-                            disableRipple>
+                                style={{ margin: '15px', width: '223px' }}
+                                variant="contained"
+                                color="primary"
+                                disableRipple
+                                onClick={(e) => this.handleOnSignin(e)}
+                            >
                                 Sign in
                             </Button>
                         </Grid>
