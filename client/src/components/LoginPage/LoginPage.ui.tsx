f85fc95e89d389a6ab09630e8cd62b137da87e4d
Roza Jiang <junzhujiang@gmail.com>
2019-06-07 14:38:47 +1000

    Finalised UI for Login Page

--- client/src/components/LoginPage/LoginPage.ui.tsx
+++ client/src/components/LoginPage/LoginPage.ui.tsx
@@ -1,9 +1,39 @@
 import * as React from 'react'
+import './styles.css'
+import { Container, Grid, TextField, Typography } from '@material-ui/core'
 
-export class LoginPage extends React.Component<any,any> {
-    public render(){
+export class LoginPage extends React.Component<any, any> {
+
+    public render() {
         return (
-            <div>I like cheesess</div>
+            <div className={'login-page'}>
+                <Container fixed>
+                    <Typography variant="button" gutterBottom>
+                        Sign into Ticket Viewer
+                    </Typography>
+                    <Grid container spacing={1}>
+                        <Grid item xs={12}>
+
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                id="outlined-name"
+                                label="Email"
+                                margin="normal"
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                id="outlined-name"
+                                label="Password"
+                                margin="normal"
+                                variant="outlined"
+                            />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </div>
         )
     }
 
