feb09870667e895082f27338a8e75db386bf51cf
Roza Jiang <junzhujiang@gmail.com>
2019-06-07 13:51:58 +1000

    Initial commit of LoginPage Component

---
+++ client/src/components/LoginPage/LoginPage.ui.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export class LoginPage extends React.Component<any,any> {
+    public render(){
+        return (
+            <div>I like cheesess</div>
+        )
+    }
+
+}
