a0bf6f193348502e120faff52b52a3c8ec3c3530
Roza Jiang <junzhujiang@gmail.com>
2019-06-09 21:59:00 +1000

    Successfully implement pagination

--- client/src/components/TicketsTable/TicketsTable.tsx
+++ client/src/components/TicketsTable/TicketsTable.tsx
@@ -4,90 +4,12 @@ import { Container, Grid, Table, TableHead, TableRow, TableCell, TableBody, Tabl
 import { ApplicationState } from '../../redux/state/ApplicationState'
 import { connect } from 'react-redux'
 import * as actions from '../../redux/actions/index'
-import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
-
-const useStyles1 = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            flexShrink: 0,
-            color: theme.palette.text.secondary,
-            marginLeft: theme.spacing(2.5),
-        },
-    }),
-);
-
-
-interface TablePaginationActionsProps {
-    count: number;
-    page: number;
-    rowsPerPage: number;
-    onChangePage: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, newPage: number) => void;
-}
-
-function TablePaginationActions(props: TablePaginationActionsProps) {
-    const classes = useStyles1();
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onChangePage } = props;
-
-    function handleFirstPageButtonClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-        onChangePage(event, 0);
-    }
-
-    function handleBackButtonClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-        onChangePage(event, page - 1);
-    }
-
-    function handleNextButtonClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-        onChangePage(event, page + 1);
-    }
-
-    function handleLastPageButtonClick(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-        onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    }
-
-    return (
-        <div className={classes.root}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="First Page"
-            >
-                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="Previous Page">
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="Next Page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="Last Page"
-            >
-                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-        </div>
-    );
-}
-
 
 interface ILoginPageState {
     email: string,
     password: string,
     simplifiedTickets: any,
     page: number,
-    setPage: number,
-    setRowsPerPage: number
 }
 
 
@@ -107,9 +29,8 @@ class TicketsTable extends React.Component<ILoginPageProps, ILoginPageState> {
             password: '',
             simplifiedTickets: {},
             page: 0,
-            setPage: 0,
-            setRowsPerPage: 5
         }
+        this.handleChangePage = this.handleChangePage.bind(this);
     }
 
     componentDidMount() {
@@ -136,8 +57,9 @@ class TicketsTable extends React.Component<ILoginPageProps, ILoginPageState> {
         event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
         newPage: number,
     ) {
+        console.log(newPage)
         this.setState({
-            setPage: newPage
+            page: newPage
         })
     }
 
@@ -146,7 +68,7 @@ class TicketsTable extends React.Component<ILoginPageProps, ILoginPageState> {
         event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
     ) {
         this.setState({
-            setRowsPerPage: parseInt(event.target.value, 10)
+            // setRowsPerPage: parseInt(event.target.value, 10)
         })
     }
 
@@ -222,7 +144,6 @@ class TicketsTable extends React.Component<ILoginPageProps, ILoginPageState> {
                                                     }}
                                                     onChangePage={this.handleChangePage}
                                                     onChangeRowsPerPage={this.handleChangeRowsPerPage}
-                                                    ActionsComponent={TablePaginationActions}
                                                 />
                                             </TableRow>
                                         </TableFooter>
