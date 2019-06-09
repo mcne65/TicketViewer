2f556bbb9aeeeb28e2847e8dc9ebc0a0a8037e4f
Roza Jiang <junzhujiang@gmail.com>
2019-06-09 11:29:45 +1000

    Displaying the whole Tickets Table

--- client/src/components/TicketsTable/TicketsTable.tsx
+++ client/src/components/TicketsTable/TicketsTable.tsx
@@ -9,13 +9,14 @@ import * as actions from '../../redux/actions/index'
 interface ILoginPageState {
     email: string,
     password: string,
+    simplifiedTickets: any
 }
 
 
 interface ILoginPageProps {
     enableErrorPage: () => void
     disableLoginPage: () => void
-    updateTicketTable: (content:any) => void
+    updateTicketTable: (content: any) => void
     tickets: any
 }
 
@@ -25,41 +26,80 @@ class TicketsTable extends React.Component<ILoginPageProps, ILoginPageState> {
         super(props)
         this.state = {
             email: '',
-            password: ''
+            password: '',
+            simplifiedTickets: {}
         }
     }
 
-    handleOnEmailChange(event: React.SyntheticEvent){
+    componentDidMount() {
+        console.log(this.props.tickets)
+        if (this.props.tickets !== undefined) {
+            const filteredTickets = this.props.tickets.map((ticket: any) => {
+                return {
+                    id: ticket.id,
+                    status: ticket.status,
+                    subject: ticket.subject,
+                    is_public: ticket.is_public,
+                    created_at: ticket.created_at,
+                    updated_at: ticket.updated_at
+                }
+
+            })
+            this.setState({
+                simplifiedTickets: filteredTickets
+            })
+        }
+    }
+
+    handleOnEmailChange(event: React.SyntheticEvent) {
         let currentTarget = event.currentTarget as HTMLInputElement
         this.setState({
             email: currentTarget.value
         })
     }
 
-    handleOnPasswordChange(event: React.SyntheticEvent){
+    handleOnPasswordChange(event: React.SyntheticEvent) {
         let currentTarget = event.currentTarget as HTMLInputElement
         this.setState({
             password: currentTarget.value
         })
     }
 
-    handleOnSignin(event: React.SyntheticEvent){
+    handleOnSignin(event: React.SyntheticEvent) {
         fetch("http://localhost:5000/")
-        .then(res => res.json())
-        .then(res => {
-            console.log(res)
-            if (res.data.error === "Couldn't authenticate you"){
-                this.props.enableErrorPage()
-            } else {
-                this.props.disableLoginPage()
-                this.props.updateTicketTable(res.data.requests)
-            }
-        })
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                if (res.data.error === "Couldn't authenticate you") {
+                    this.props.enableErrorPage()
+                } else {
+                    this.props.disableLoginPage()
+                    this.props.updateTicketTable(res.data.requests)
+                }
+            })
 
     }
 
     public render() {
         console.log(this.props.tickets)
+        let filteredTickets: any
+
+        if (this.props.tickets !== undefined) {
+            filteredTickets = this.props.tickets.map((ticket: any) => {
+                return {
+                    id: ticket.id,
+                    status: ticket.status,
+                    subject: (ticket.subject.length > 50) ? ticket.subject.substring(0, 50) + '...' : ticket.subject,
+                    is_public: ticket.is_public,
+                    created_at: ticket.created_at,
+                    updated_at: ticket.updated_at
+                }
+            })
+            console.log(filteredTickets)
+        } else {
+            filteredTickets = undefined
+        }
+
         return (
             <div className={'login-page'}>
                 <Container fixed>
@@ -68,23 +108,25 @@ class TicketsTable extends React.Component<ILoginPageProps, ILoginPageState> {
                             <Table>
                                 <TableHead>
                                     <TableRow>
-                                        <TableCell> Hello </TableCell>
-                                        <TableCell align="right"> What </TableCell>
-                                        <TableCell align="right"> What </TableCell>
-                                        <TableCell align="right"> What </TableCell>
-                                        <TableCell align="right"> What </TableCell>
+                                        <TableCell> ID </TableCell>
+                                        <TableCell align="right"> Status </TableCell>
+                                        <TableCell> Subject </TableCell>
+                                        <TableCell align="right"> Created at </TableCell>
+                                        <TableCell align="right"> Updated at </TableCell>
                                     </TableRow>
                                 </TableHead>
                                 <TableBody>
-                                    <TableRow key={'hello'}>
-                                        <TableCell component = "th" scope="row">
-                                            name
-                                        </TableCell>
-                                        <TableCell align="right">hie</TableCell>
-                                        <TableCell align="right">hie</TableCell>
-                                        <TableCell align="right">hie</TableCell>
-                                        <TableCell align="right">hie</TableCell>
-                                    </TableRow>
+                                    {(filteredTickets === undefined)?null:filteredTickets.map((ticket: any) => (
+                                        <TableRow key={ticket.id}>
+                                            <TableCell component="th" scope="row">
+                                                {ticket.id}
+                                            </TableCell>
+                                            <TableCell align="right">{ticket.status}</TableCell>
+                                            <TableCell>{ticket.subject}</TableCell>
+                                            <TableCell align="right">{ticket.created_at}</TableCell>
+                                            <TableCell align="right">{ticket.updated_at}</TableCell>
+                                        </TableRow>
+                                    ))}
                                 </TableBody>
                             </Table>
                         </Grid>
@@ -97,20 +139,20 @@ class TicketsTable extends React.Component<ILoginPageProps, ILoginPageState> {
 }
 
 
-function mapStateToProps({isUserValid, tickets}: ApplicationState){
+function mapStateToProps({ isUserValid, tickets }: ApplicationState) {
     return {
-      isUserValid,
-      tickets
+        isUserValid,
+        tickets
     }
-  }
+}
 
-function mapDispatchToProps(dispatch:any){
+function mapDispatchToProps(dispatch: any) {
     return {
         enableErrorPage: () => dispatch(actions.enableErrorPage()),
-        updateTicketTable: (content:any) => dispatch(actions.updateTicketTable(content)),
+        updateTicketTable: (content: any) => dispatch(actions.updateTicketTable(content)),
         disableLoginPage: () => dispatch(actions.disableLoginPage()),
     }
 }
-  
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(TicketsTable)
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(TicketsTable)
--- server/app.js
+++ server/app.js
@@ -1,19 +1,21 @@
+const variables = require('../var')
 const express = require('express')
 const app = express()
 const port = 5000
 const fetch = require('node-fetch')
 const base64 = require('base-64')
 const cors = require('cors')
-const username = 'junzhujiang@gmail.com'
-const password = 'dougieishot'
+
 
 app.use(cors())
 app.get('/', (req, res) => {
     const apiUrl = 'https://rozajaybird.zendesk.com/api/v2/requests.json'
+    // const apiUrl = 'https://rozajaybird.zendesk.com/api/v2/requests/2.json'
+    
     fetch(apiUrl, {
         method:'GET',
         headers: {
-            'Authorization': 'Basic ' + base64.encode(username + ":" + password)
+            'Authorization': 'Basic ' + base64.encode(variables.email + ":" + variables.password)
         }
     })
     .then(res => res.json())
@@ -25,4 +27,4 @@ app.get('/', (req, res) => {
     })
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`))
+app.listen(port, () => console.log(`Example app listening on port ${variables.email}`))
