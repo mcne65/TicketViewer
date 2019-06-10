e9460079681e20136d5ff77579adabddcac534f4
Roza Jiang <junzhujiang@gmail.com>
2019-06-10 15:49:44 +1000

    Planning the information of single ticket

--- client/src/components/SingleTicket/SingleTicket.tsx
+++ client/src/components/SingleTicket/SingleTicket.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import './styles.css'
-import { CardActions, Typography, Button, Card, CardContent, Icon, Tooltip } from '@material-ui/core'
+import { CardActions, Typography, Button, Card, CardContent, Icon, Tooltip, Grid } from '@material-ui/core'
 import { ApplicationState } from '../../redux/state/ApplicationState'
 import { connect } from 'react-redux'
 import * as actions from '../../redux/actions/index'
@@ -31,27 +31,50 @@ class SingleTicket extends React.Component<ISingleTicketProps> {
                             gutterBottom align="left"
                             variant="h5"
                         >
-                            Ticket Details
-                        {true ? 
-                        <Tooltip title="Not solvable by me" placement="top">   
-                        <Icon style={{color: 'red', marginLeft: '2px'}}>
-                            cancel
+                            Ticket (ID: 1) Details
+                        {true ?
+                                <Tooltip title="Not solvable by me" placement="top">
+                                    <Icon style={{ color: 'red', marginLeft: '2px' }}>
+                                        cancel
                         </Icon>
-                        </Tooltip>:
-                        <Tooltip title="Solvable by me" placement="top">   
-                        <Icon style={{color: 'green', marginLeft: '2px'}}>
-                            check_circle
+                                </Tooltip> :
+                                <Tooltip title="Solvable by me" placement="top">
+                                    <Icon style={{ color: 'green', marginLeft: '2px' }}>
+                                        check_circle
                         </Icon>
-                        </Tooltip>}
+                                </Tooltip>}
                         </Typography>
-                        
+
                     </CardContent>
+                    <Grid container spacing={1}>
+                        <Grid item xs={6}>
+                            Status
+                            </Grid>
+                        <Grid item xs={6}>
+                            Requester
+                            </Grid>
+                        <Grid item xs={6}>
+                            Organisation
+                        </Grid>
+                        <Grid item xs={6}>
+                            Assignee
+                        </Grid>
+                        <Grid item xs={12}>
+                            Labels
+                        </Grid>
+                        <Grid item xs={12}>
+                            Subject
+                        </Grid>
+                        <Grid item xs={12}>
+                            Description
+                        </Grid>
+                    </Grid>
                     <CardActions>
-                        <Button 
-                        variant="outlined"
-                        color="primary"
-                        size="medium"
-                        onClick={this.props.enableTicketsTablePage}
+                        <Button
+                            variant="outlined"
+                            color="primary"
+                            size="medium"
+                            onClick={this.props.enableTicketsTablePage}
                         > Back to all tickets </Button>
                     </CardActions>
                 </Card>
