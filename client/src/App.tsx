715e5489ca7a61ff9c1eb3d3381c33c4b7990751
Roza Jiang <junzhujiang@gmail.com>
2019-06-12 10:21:57 +1000

    Start to write UI tests

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import  LoginPage  from './components/LoginPage/LoginPage'
+import LoginPage  from './components/LoginPage/LoginPage'
 import TicketsTable from './components/TicketsTable/TicketsTable'
-import ErrorPage  from './components/ErrorPage/ErrorPage.ui'
+import ErrorPage  from './components/ErrorPage/ErrorPage'
 import { Header } from './components/Header/Header.ui'
 import { connect } from 'react-redux'
 import { ApplicationState } from './redux/state/ApplicationState'
