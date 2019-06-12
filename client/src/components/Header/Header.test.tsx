fb480b8b41b495e4972e93d4217e21fa0387366f
Roza Jiang <junzhujiang@gmail.com>
2019-06-12 11:10:55 +1000

    Added Header UI test

---
+++ client/src/components/Header/Header.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import {Header} from './Header.ui';
+
+describe('Test for App component', () => {
+   it('renders without crashing', () => {
+      shallow(<Header />)
+    });
+});
